a0f6411fc8ef3a6f7360867c2525cf5f20f873a5
rupanjanas <136469439+rupanjanas@users.noreply.github.com>
2025-05-05 17:39:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,77 @@
+import requests
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
+
+STOCK_API_KEY = os.getenv("STOCK_API_KEY")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+TWILIO_NUMBER = os.getenv("TWILIO_NUMBER")
+VERIFIED_NUMBER = os.getenv("VERIFIED_NUMBER")
+
+STOCK_NAME = "NVDA"
+COMPANY_NAME = "NVIDIA Corporation"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
+
+diff_percent = round((difference / float(day_before_yesterday_closing_price)) * 100)
+print(diff_percent)
+
+
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+19566920944",
+            to="+919051744016"
+        )
